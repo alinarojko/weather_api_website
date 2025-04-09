c51f68f2319de08ea3ffaa9f648767a32420012b
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-09 21:17:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas
+
+app = Flask("__name__")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    df = pandas.read_csv("")
+    return render_template("about.html")
+
+
+app.run(debug=True)
