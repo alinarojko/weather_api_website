93cb85bdadc67e7048c0ceeb53e05ed07832799e
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-11 11:20:46 +0100

    Add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -15,14 +15,44 @@ def home():
     print(stations.head())
     return render_template("home.html", data=stations.to_html())
 
+# Case to show all information , for one station for one "date" - user input
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
+    # Create a filename for current station
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+
+    # Load the data into a dataframe, skip first 20 rows, parse the date column , to be treated like date
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].sqveeze() / 10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+# Case to show all information , for excact "station"  for all dates - user input
+@app.route("/api/vi/<stations>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    # Convert a dataframe to a dictionary
+    result = df.to_dict(orient="records")
+    return result
+
+
+# Case to show all information , for excact "station"  for one year - user input
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    # Convert date column to a str
+    df["    DATE"] = df["    DATE"].astype(str)
+
+    # Collecting respond , convert dataframe with data from that one year to the dictionary. add orientation to records to be
+    # in the json format
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
